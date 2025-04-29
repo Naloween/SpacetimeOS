b76c78d8d9cc6a5587dbbfb24e1c992a8f7955bd
Naloween <40539093+Naloween@users.noreply.github.com>
2025-04-29 22:19:28 +0200

    working kernel prototype (starting paging)

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(spacetime_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use bootloader::{BootInfo, entry_point};
+use core::panic::PanicInfo;
+use spacetime_os::{memory::translate_addr, println};
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::VirtAddr;
+
+    println!("Hello World{}", "!");
+    spacetime_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    spacetime_os::hlt_loop();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    spacetime_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    spacetime_os::test_panic_handler(info)
+}
